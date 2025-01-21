06032bac47ecb3ec23351aa25152049d46d19ef6
DRAGKOZ <skdkuriel@hotmail.com>
2025-01-21 16:03:34 -0600

    fix home screen

--- app/Views/main.php
+++ app/Views/main.php
@@ -1,20 +1,81 @@
-<div class="card">
+<div class="card" style="padding-left: 0; padding-right: 0;">
 	<div class="card-body pt-0">
 		<ul class="nav nav-pills nav-justified" role="tablist">
 			<li class="nav-item waves-effect waves-light" role="presentation">
 				<a class="nav-link active" data-bs-toggle="tab" href="#test1" role="tab" aria-selected="true" tabindex="-1">Inicio</a>
 			</li>
 			<li class="nav-item waves-effect waves-light" role="presentation">
-				<a
-						class="nav-link" data-bs-toggle="tab" href="#test2" role="tab" aria-selected="false" tabindex="-1" onclick="GetDisposiciones()">Disposiciones</a>
+				<a class="nav-link" data-bs-toggle="tab" href="#test2" role="tab" aria-selected="false" tabindex="-1" onclick="GetDisposiciones()">Disposiciones</a>
 			</li>
 			<li class="nav-item waves-effect waves-light" role="presentation">
 				<a class="nav-link" data-bs-toggle="tab" href="#test3" role="tab" aria-selected="false" tabindex="-1">Notificaciones</a>
 			</li>
 		</ul>
 		<div class="tab-content">
-			<div class="tab-pane active p-3" id="test1" role="tabpanel">
-				<h3 style="text-align: center">¿Cuánto dinero necesitas?</h3>
+			<div class="tab-pane active p-3" id="test1" role="tabpanel" style="padding-top: 10px !important;">
+				<h3 style="text-align: center; margin-bottom: 0">¿Cuánto dinero necesitas?</h3>
+				<div class="container-fluid justify-content-center card cardback">
+					<div class="row justify-content-center" style="padding: 10px 5px 0 5px">
+						<div class="col-xs-3 col-md-3"><h5>Disposiciónes por:</h5></div>
+						<div class="col-xs-2 col-md-2"><h4 id="dXD">Día: 1/2</h4></div>
+						<div class="col-xs-3 col-md-3"><h4 id="dXQ">Quincena: 1/5</h4></div>
+						<div class="col-xs-2 col-md-2"><h4 id="dXM">Mes: 1/10</h4></div>
+					</div>
+					<div class="row ">
+						<div class="col-xs-6 col-sm-6">
+							<div class="card cardback bordercard">
+								<div class="" style="padding: 5px 0 0 0">
+									<h5 class="card-title text-center" style="margin: 0;">Dias trabajados</h5>
+								</div>
+								<div class="" style="padding: 2px">
+									<h2 class="text-center" style="font-weight: bold"><img src="assets/img/calendar.png" style="height: 1.7rem"> 13</h2>
+								</div>
+							</div>
+						</div>
+						<div class="col-xs-6 col-sm-6">
+							<div class="card cardback bordercard">
+								<div class="" style="padding: 5px 0 0 0">
+									<h5 class="card-title text-center" style="margin: 0">Monto disponible</h5>
+								</div>
+								<div class="" id="DashAvailable" style="padding: 2px">
+									<h2 class="text-center" style="font-weight: bold">$ 2,500.00</h2>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-12 col-sm-12">
+							<div class="card cardback bordercard">
+								<div class="" style="padding: 5px 0 0 0">
+									<h3 class="card-title text-center" style="margin: 0;">Monto</h3>
+								</div>
+								<div class="card-body text-center justify-content-center" style="padding-top: 0">
+									<div class="row justify-content-center">
+										<div class="col-xs-3 col-md-8" style="font-size: 2.5rem">
+											<output id="outRequestAmount" class="dinero" style="font-weight: bold">$ 4,000</output>
+											<input type="hidden" id="MontoReal" value="">
+										</div>
+										<div class="col-xs-3 col-md-8">
+											<input
+													id="requestAmount" name="requestAmount" type="range" value="" min="" max="" step="10"
+													oninput="document.getElementById('MontoReal').value = this.value; outRequestAmount.value= '$ ' + Intl.NumberFormat('en-US').format(value); this.style.cssText = '';" />
+										</div>
+									</div>
+									<div class="row justify-content-center" style="margin-top: 25px">
+										<div class="col-xs-3 col-sm-10 ">
+											<button
+													id="reqPay" name="reqPay" type="submit" class="btn"
+													onclick="document.getElementById('exampleModalScrollable').style.display='block'"
+													style="width: 100%; color: var(--title-color) !important; font-size: 1rem; height: 2.5rem; letter-spacing: 0.1rem; margin-bottom: 1.5rem; background-color: #FF9400 !important;"
+													data-bs-toggle="modal" data-bs-target="#exampleModalScrollable">Solicitar
+											</button>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 				<!--	<div class="row justify-content-center">
 						<div class="col-md-3 card cardback bordercard" style="height: 80px">
 							<div class="card-body pt-1">
@@ -49,19 +110,20 @@
 							</div>
 						</div>
 					</div>-->
-				<div class="row justify-content-center" style="height: 40px">
-					<div class="col-md-10 card cardback " style=" padding-left: 0; padding-right: 0; margin-bottom: 5px">
-						<div class="card-body pt-1" style="padding-bottom: .1rem">
-							<div class="row">
-								<div class="col-md-3"><h5>Disposiciónes por:</h5></div>
-								<div class="col-md-2"><h4 id="dXD">Día: 1/2</h4></div>
-								<div class="col-md-3"><h4 id="dXQ">Quincena: 1/5</h4></div>
-								<div class="col-md-2"><h4 id="dXM">Mes: 1/10</h4></div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="row justify-content-center" style="height: 90px">
+<!--				<div class="row justify-content-center" style="height: 40px">-->
+<!--					<div class="col-md-10 card cardback " style=" padding-left: 0; padding-right: 0; margin-bottom: 5px">-->
+<!--						<div class="card-body pt-1" style="padding-bottom: .1rem">-->
+<!--							<div class="row">-->
+<!--								<div class="col-md-3"><h5>Disposiciónes por:</h5></div>-->
+<!--								<div class="col-md-2"><h4 id="dXD">Día: 1/2</h4></div>-->
+<!--								<div class="col-md-3"><h4 id="dXQ">Quincena: 1/5</h4></div>-->
+<!--								<div class="col-md-2"><h4 id="dXM">Mes: 1/10</h4></div>-->
+<!--							</div>-->
+<!--							-->
+<!--						</div>-->
+<!--					</div>-->
+<!--				</div>-->
+<!--				<div class="row justify-content-center" style="height: 90px">-->
 					<!--<div class="col-md-4 card cardback bordercard" style="height: 100px">
 						<div class="" >
 							<div class="card-body pt-1">
@@ -73,57 +135,55 @@
 							</div>
 						</div>
 					</div>-->
-					<div class="col-md-5 card " style="height: 80px; padding-left: 0; padding-bottom: 2px">
-						<div class="card cardback bordercard" style="height: 80px; margin-bottom: 5px">
-							<div class="card-body pt-1" style="padding-bottom: 5px">
-								<div class="card-header cardback" style="padding-bottom: 0.1rem;padding-top: .2rem;">
-									<h3 class="card-title text-center">Dias trabajados</h3>
-								</div>
-								<div class="card-body pt-0" id="DashDays" style="padding-bottom: 0">
-									<h2 class="text-center" style="font-weight: bold"><img src="assets/img/calendar.png" style="height: 1.7rem"> 13</h2>
-								</div>
-							</div>
-						</div>
-					</div>
-				
-					<div class="col-md-5 card cardback bordercard" style="height: 80px; margin-bottom: 5px; padding-bottom: 2px">
-						<div class="card-body pt-1" style="padding-bottom: 5px">
-							<div class="card-header cardback" style="padding-bottom: 0.1rem; padding-top: .2rem;">
-								<h3 class="card-title text-center">Monto disponible</h3>
-							</div>
-							<div class="card-body pt-0" id="DashAvailable" style="padding-top: 0; padding-bottom: 0">
-								<h2 class="text-center" style="font-weight: bold">$ 2,500.00</h2>
-							</div>
-						</div>
-					</div>
-				</div>
-				
-				<div class="row justify-content-center">
-					<div class="col-md-10 card cardback bordercard">
-						<h3 class="text-center" style="font-size: 1.5rem; margin-top: 1.5rem; margin-bottom: 0;">Monto</h3>
-						<div class="col-md-12">
-							<div class="card-body text-center" style="padding-top: 0">
-								<div class="row justify-content-center">
-									<div class="col-md-8" style="font-size: 2.5rem">
-										<output id="outRequestAmount" class="dinero" style="font-weight: bold">$ 4,000</output>
-										<input type="hidden" id="MontoReal" value="">
-									</div>
-									<div class="col-md-8">
-										<input
-												id="requestAmount" name="requestAmount" type="range" value="" min="" max="" step="10"
-												oninput="document.getElementById('MontoReal').value = this.value; outRequestAmount.value= '$ ' + Intl.NumberFormat('en-US').format(value); this.style.cssText = '';" />
-									</div>
-								</div>
-							</div>
-							<button
-									id="reqPay" name="reqPay" type="submit" class="btn"
-									onclick="document.getElementById('exampleModalScrollable').style.display='block'"
-									style="width: 100%; color: var(--title-color) !important; font-size: 1rem; height: 2.5rem; letter-spacing: 0.1rem; margin-bottom: 1.5rem; background-color: #FF9400 !important;"
-									data-bs-toggle="modal" data-bs-target="#exampleModalScrollable">Solicitar
-							</button>
-						</div>
-					</div>
-				</div>
+<!--					<div class="col-md-5 card " style="height: 80px; padding-left: 0; padding-bottom: 2px">-->
+<!--						<div class="card cardback bordercard" style="height: 80px; margin-bottom: 5px">-->
+<!--							<div class="card-body pt-1" style="padding-bottom: 5px">-->
+<!--								<div class="card-header cardback" style="padding-bottom: 0.1rem;padding-top: .2rem;">-->
+<!--									<h3 class="card-title text-center">Dias trabajados</h3>-->
+<!--								</div>-->
+<!--								<div class="card-body pt-0" id="DashDays" style="padding-bottom: 0">-->
+<!--									<h2 class="text-center" style="font-weight: bold"><img src="assets/img/calendar.png" style="height: 1.7rem"> 13</h2>-->
+<!--								</div>-->
+<!--							</div>-->
+<!--						</div>-->
+<!--					</div>-->
+					
+<!--					<div class="col-md-5 card cardback bordercard" style="height: 80px; margin-bottom: 5px; padding-bottom: 2px">-->
+<!--						<div class="card-body pt-1" style="padding-bottom: 5px">-->
+<!--							<div class="card-header cardback" style="padding-bottom: 0.1rem; padding-top: .2rem;">-->
+<!--								<h3 class="card-title text-center">Monto disponible</h3>-->
+<!--							</div>-->
+<!--							<div class="card-body pt-0" id="DashAvailable" style="padding-top: 0; padding-bottom: 0">-->
+<!--								<h2 class="text-center" style="font-weight: bold">$ 2,500.00</h2>-->
+<!--							</div>-->
+<!--						</div>-->
+<!--					</div>-->
+<!--				<div class="row justify-content-center">-->
+<!--					<div class="col-md-10 card cardback bordercard">-->
+<!--						<h3 class="text-center" style="font-size: 1.5rem; margin-top: 1.5rem; margin-bottom: 0;">Monto</h3>-->
+<!--						<div class="col-md-12">-->
+<!--							<div class="card-body text-center" style="padding-top: 0">-->
+<!--								<div class="row justify-content-center">-->
+<!--									<div class="col-md-8" style="font-size: 2.5rem">-->
+<!--										<output id="outRequestAmount" class="dinero" style="font-weight: bold">$ 4,000</output>-->
+<!--										<input type="hidden" id="MontoReal" value="">-->
+<!--									</div>-->
+<!--									<div class="col-md-8">-->
+<!--										<input-->
+<!--												id="requestAmount" name="requestAmount" type="range" value="" min="" max="" step="10"-->
+<!--												oninput="document.getElementById('MontoReal').value = this.value; outRequestAmount.value= '$ ' + Intl.NumberFormat('en-US').format(value); this.style.cssText = '';" />-->
+<!--									</div>-->
+<!--								</div>-->
+<!--							</div>-->
+<!--							<button-->
+<!--									id="reqPay" name="reqPay" type="submit" class="btn"-->
+<!--									onclick="document.getElementById('exampleModalScrollable').style.display='block'"-->
+<!--									style="width: 100%; color: var(--title-color) !important; font-size: 1rem; height: 2.5rem; letter-spacing: 0.1rem; margin-bottom: 1.5rem; background-color: #FF9400 !important;"-->
+<!--									data-bs-toggle="modal" data-bs-target="#exampleModalScrollable">Solicitar-->
+<!--							</button>-->
+<!--						</div>-->
+<!--					</div>-->
+<!--				</div>-->
 			</div>
 			<div class="tab-pane p-3" id="test2" role="tabpanel">
 				<div class="table-responsive">
@@ -149,7 +209,7 @@
 			</div>
 			<div class="tab-pane p-3 show" id="test3" role="tabpanel">
 				<p class="text-muted mb-0">
-					No se encontraron pagos realizados
+					No se encontraron notificaciones.
 				</p>
 			</div>
 		</div>
@@ -157,9 +217,7 @@
 </div>
 
 <!--modal-->
-<div
-		class="modal fade" id="exampleModalScrollable" tabindex="-1" aria-labelledby="exampleModalScrollableTitle" style="display: none;" aria-modal="true"
-		role="dialog">
+<div class="modal fade" id="exampleModalScrollable" tabindex="-1" aria-labelledby="exampleModalScrollableTitle" style="display: none;" aria-modal="true" role="dialog">
 	<div class="modal-dialog modal-dialog-scrollable" role="document">
 		<div class="modal-content">
 			<div class="modal-header">
