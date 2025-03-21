e6a73c18e63d450aaa525b280dbceeca7d3364fd
drakoz <skdkuriel@hotmail.com>
2025-03-21 10:36:29 -0600

    fix user on tutorials

--- app/Controllers/FaqsController.php
+++ app/Controllers/FaqsController.php
@@ -10,9 +10,18 @@
 			if ( !$this->validateSession () ) {
 				return redirect ( 'signIn' );
 			}
+			$session = session ();
+			$user = $session->get ( 'user' )[ 'data' ];
+			$name = $user[ 'name' ].' '.$user[ 'last_name' ];
+			$initials = substr ( $user[ 'name' ], 0, 1 ).substr ( $user[ 'last_name' ], 0, 1 );
+			$data[ 'iniciales' ] = $initials;
+			$data[ 'name' ] = $name;
+			$data[ 'company' ] = $user[ 'short_name' ];
+			$data[ 'session' ] = TRUE;
+			$data [ 'title' ] = 'SolveExpress | Perfil';
 			if ( str_contains ( service ( 'request' )->getHeaderLine ( 'Accept-Encoding' ), 'gzip' ) ) {
 				$this->response->setHeader('Content-Encoding', 'gzip');
-				$this->response->setBody(gzencode(view('Company/tutorials' )));
+				$this->response->setBody(gzencode(view('Company/tutorials', $data )));
 				return $this->response;
 			}
 			return view ( 'Company/tutorials' );
